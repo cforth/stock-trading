9f980beffbb9f701e6bc9e580d2d042d061ab1a0
xiaohao@cforth <cforth@163.com>
2014-03-08 14:22:02 +0800

    注册页面修改为连接cf_stock

--- public/register.php
+++ public/register.php
@@ -12,18 +12,20 @@
             die("Could not connect to database");
 
         // select database
-        if (mysql_select_db("cfstocks", $connection) === false)
+        if (mysql_select_db("cf_stock", $connection) === false)
             die("Could not select database");
 
-        $name = sprintf("SELECT 1 FROM users WHERE user='%s'", mysql_real_escape_string($_POST["user"]));
+        $name = sprintf("SELECT 1 FROM `stock_user` WHERE `userName` = '%s'", mysql_real_escape_string($_POST["user"]));
 
         $result = mysql_query($name);
         if (mysql_num_rows($result) == 0) {
           // prepare SQL
-          $sql = sprintf("INSERT INTO `users`(`user`, `pass`) VALUES ('%s' , AES_ENCRYPT('%s', '%s'))",
+          $sql = sprintf("INSERT INTO `stock_user` (`userName`, `userPassword`, `userEmail`, `userLevel`) VALUES ('%s' , AES_ENCRYPT('%s', '%s'), '%s', '%d')",
                        mysql_real_escape_string($_POST["user"]),
                        mysql_real_escape_string($_POST["pass1"]),
-                       mysql_real_escape_string($_POST["pass1"]));
+                       mysql_real_escape_string($_POST["pass1"]),
+                       mysql_real_escape_string($_POST["email"]),
+                       1);
 
           // execute query
           $result = mysql_query($sql);
@@ -50,52 +52,46 @@
   require '../module/header.php';
 ?>
 
-    <script>
+  <script>
 
-        function validate()
-        {
-            if (document.forms.registration.user.value =="")
-            {
-                alert("用户名不能为空");
-                return false;
-            }
-            else if (document.forms.registration.pass1.value == "")
-            {
-                alert("密码不能为空");
-                return false;
-            }
-            else if (document.forms.registration.pass1.value != document.forms.registration.pass2.value)
-            {
-                alert("两次必须输入同样的密码");
-                return false;
-            }
-            return true;
-        }
+    function validate()  {
+      if (document.forms.registration.user.value == "") {
+        alert("用户名不能为空");
+        return false;
+      }
+      else if (document.forms.registration.pass1.value == "") {
+        alert("密码不能为空");
+        return false;
+      }
+      else if (document.forms.registration.pass1.value != document.forms.registration.pass2.value) {
+        alert("两次必须输入同样的密码");
+        return false;
+      }
+      else if (document.forms.registration.email.value == "") {
+        alert("Email地址不能为空");
+        return false;
+      }
 
-        </script>
+      return true;
+    }
+
+  </script>
 
     <br />
     <form action="<?php  print($_SERVER["PHP_SELF"]) ?>" method="post" name="registration" onsubmit="return validate();">
-      <table>
-        <tr>
-          <td>用户名:</td>
-          <td>
-            <input name="user" type="text"></td>
-        </tr>
-        <tr>
-          <td>设置密码:</td>
-          <td><input name="pass1" type="text"></td>
-        </tr>
-        <tr>
-        <tr>
-          <td>确认密码:</td>
-          <td><input name="pass2" type="text"></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><input type="submit" class="push" value="注册"></td>
-        </tr>
-      </table>      
+    用户名:
+    <input name="user" type="text">
+    <br />
+    设置密码:
+    <input name="pass1" type="text">
+    <br />
+    确认密码:
+    <input name="pass2" type="text">
+    <br />
+    Email:
+    <input name="email" type="text">
+    <br />
+    <input type="submit" class="push" value="注册">
     </form>
 
     <p><?php print($prompt) ?></p>
